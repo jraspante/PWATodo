6d4914a8ceee91cc0735c1eec791358fd390b8ee
Gustavo Loschi Salomão <gustavo.salomao@sga.pucminas.br>
2019-05-04 16:48:08 -0300

    Commit inicial

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,30 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FormsModule } from '@angular/forms';
+
 import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
+import { MatToolbarModule, MatButtonModule, MatIconModule, MatInputModule, MatListModule } from "@angular/material";
+import { FlexLayoutModule } from "@angular/flex-layout";
+import { TodoComponent } from './todo/todo.component';
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TodoComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FlexLayoutModule,
+    MatToolbarModule,
+    MatButtonModule,
+    MatIconModule,
+    MatInputModule,
+    MatListModule,
+    BrowserAnimationsModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
